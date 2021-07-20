ed2807705adbf14d5f9968725c03aa235f5c1c08
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-20 15:32:59 +0530

    Final commit

--- argv.js
+++ argv.js
@@ -8,7 +8,10 @@ function add(a,b)
 }
  var sample = add(arguments[2],arguments[3]);
  console.log(sample);
-
+ process.argv.forEach((val,index) => {  //why Index comes as second argument here
+    console.log(`${index}: ${val}`)
+  })
  const args = (process.argv.slice(2))
  console.log('After slice');
 console.log(args)
+//node argv.js 2 3 Indhu Hello
