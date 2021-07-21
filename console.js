f5087cd9ce46528132c8c5e36d3abae14253ce89
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-21 18:02:59 +0530

    commit

--- console.js
+++ console.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk')
 console.log("Hello world");
 console.log('Hello %s', 'world');
 console.error(new Error('Whoops, something bad happened')); // will print error stack
@@ -35,3 +36,21 @@ console.countReset('orange'); // It reset the string orange count to 0 here
 oranges.forEach(fruit => {
   console.count(fruit)      // so here again it print from 1 and 2
 })
+
+const function2 = () => console.trace()
+const function1 = () => function2()
+function1()
+
+
+const doSomething = () => console.log('test')
+const measureDoingSomething = () => {
+  console.time('doSomething()')
+  //do something, and measure the time it takes
+  doSomething()
+  console.timeEnd('doSomething()')
+}
+measureDoingSomething()
+
+console.log('\x1b[33m%s\x1b[0m', 'hi!')
+
+console.log(chalk.red('Hi!'))
